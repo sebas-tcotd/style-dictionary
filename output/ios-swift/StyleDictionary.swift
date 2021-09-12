f63b925490fb432c4f0811c34eeeee2c78001dc0
sebas-tcotd <sebas-tcotd@users.noreply.github.com>
2021-09-12 23:25:23 +0000

    chore: add design tokens update

---
+++ output/ios-swift/StyleDictionary.swift
@@ -0,0 +1,73 @@
+
+//
+// StyleDictionary.swift
+//
+
+// Do not edit directly
+// Generated on Sun, 12 Sep 2021 23:25:22 GMT
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let gradientButtonBorderGradientGradientType = linear
+    public static let gradientButtonBorderGradientOpacity = 1
+    public static let gradientButtonBorderGradientStops0Color = rgba(186, 191, 195, 1)
+    public static let gradientButtonBorderGradientStops0Position = 0.86
+    public static let gradientButtonBorderGradientStops1Color = rgba(187, 191, 195, 1)
+    public static let gradientButtonBorderGradientStops1Position = 1
+    public static let gradientTextInputBorderGradientType = linear
+    public static let gradientTextInputBorderOpacity = 1
+    public static let gradientTextInputBorderStops0Color = rgba(167, 172, 177, 1)
+    public static let gradientTextInputBorderStops0Position = 0
+    public static let gradientTextInputBorderStops1Color = rgba(186, 191, 195, 1)
+    public static let gradientTextInputBorderStops1Position = 0.1
+    public static let gridSpacingPadding160Alignment = min
+    public static let gridSpacingPadding160Count = 1
+    public static let gridSpacingPadding160GutterSize = 16
+    public static let gridSpacingPadding160Offset = 16
+    public static let gridSpacingPadding160Pattern = columns
+    public static let gridSpacingPadding160SectionSize = 1
+    public static let gridSpacingPadding161Alignment = max
+    public static let gridSpacingPadding161Count = 1
+    public static let gridSpacingPadding161GutterSize = 16
+    public static let gridSpacingPadding161Offset = 16
+    public static let gridSpacingPadding161Pattern = columns
+    public static let gridSpacingPadding161SectionSize = 1
+    public static let gridSpacingPadding162Alignment = min
+    public static let gridSpacingPadding162Count = 1
+    public static let gridSpacingPadding162GutterSize = 16
+    public static let gridSpacingPadding162Offset = 16
+    public static let gridSpacingPadding162Pattern = rows
+    public static let gridSpacingPadding162SectionSize = 1
+    public static let gridSpacingPadding163Alignment = max
+    public static let gridSpacingPadding163Count = 1
+    public static let gridSpacingPadding163GutterSize = 16
+    public static let gridSpacingPadding163Offset = 16
+    public static let gridSpacingPadding163Pattern = rows
+    public static let gridSpacingPadding163SectionSize = 1
+    public static let gridSpacingPadding200Alignment = max
+    public static let gridSpacingPadding200Count = 1
+    public static let gridSpacingPadding200GutterSize = 20
+    public static let gridSpacingPadding200Offset = 20
+    public static let gridSpacingPadding200Pattern = columns
+    public static let gridSpacingPadding200SectionSize = 1
+    public static let gridSpacingPadding201Alignment = max
+    public static let gridSpacingPadding201Count = 1
+    public static let gridSpacingPadding201GutterSize = 20
+    public static let gridSpacingPadding201Offset = 20
+    public static let gridSpacingPadding201Pattern = rows
+    public static let gridSpacingPadding201SectionSize = 1
+    public static let gridSpacingPadding202Alignment = min
+    public static let gridSpacingPadding202Count = 1
+    public static let gridSpacingPadding202GutterSize = 20
+    public static let gridSpacingPadding202Offset = 20
+    public static let gridSpacingPadding202Pattern = columns
+    public static let gridSpacingPadding202SectionSize = 1
+    public static let gridSpacingPadding203Alignment = min
+    public static let gridSpacingPadding203Count = 1
+    public static let gridSpacingPadding203GutterSize = 20
+    public static let gridSpacingPadding203Offset = 20
+    public static let gridSpacingPadding203Pattern = rows
+    public static let gridSpacingPadding203SectionSize = 1
+}
